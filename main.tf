818906c0ed54b0ce27c295e6c447fd2cad13b84f
Felipe Ferreira Gomes <felipe.gomes@pinauto.com.br>
2025-08-14 09:15:56 -0300

    1º pipeline de teste

--- main.tf
+++ main.tf
@@ -51,6 +51,7 @@ resource "aws_subnet" "subnet_pub" {
 resource "aws_security_group" "acesso_webserver" {
   name        = "acesso_ssh_http"
   description = "Permite acesso SSH e HTTP a instância"
+  vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
     description = "SSH"
