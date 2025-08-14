dc6649f65498f441acd71db79c31e5f5bd3d0267
Felipe Ferreira Gomes <felipe.gomes@pinauto.com.br>
2025-08-14 09:32:40 -0300

    3º pipeline de teste

--- main.tf
+++ main.tf
@@ -14,7 +14,8 @@ provider "aws" {
 resource "aws_instance" "webserver" {
   ami                    = "ami-0de716d6197524dd9"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.acesso_webserver.id]
+  subnet_id = aws_subnet.subnet_pub.id
+  vpc_security_group_ids = aws_security_group.acesso_webserver.id
   user_data = file("ec2-data.sh")
 
   tags = {
